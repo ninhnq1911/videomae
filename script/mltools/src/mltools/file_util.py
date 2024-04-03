f59e05d5ada53b09d5608662afc707ce34fab204
ninhnq1911 <ninhnguyendx779@gmail.com>
2024-04-04 02:40:31 +0700

    Add ucf 101 subset info

--- script/mltools/src/mltools/file_util.py
+++ script/mltools/src/mltools/file_util.py
@@ -1,4 +1,5 @@
 import os
+import glob
 from pathlib import Path
 
 DEFAULT_COLAB_MOUNTED_FOLDER = os.getenv("DEFAULT_COLAB_MOUNTED_FOLDER", "DATA")
@@ -16,3 +17,20 @@ def make_dir(name: Path | str):
 
 def get_resource(name: str) -> str:
     return make_dir(os.path.join(DEFAULT_COLAB_MOUNTED_FOLDER, PROJECT_NAME, name))
+
+
+def resolve_path(path: str | Path) -> str:
+    return Path(path).resolve().as_posix()
+
+
+def get_subfolders(dir_path: str | Path) -> list[str]:
+    if dir_path is None:
+        return []
+    return [f.name for f in Path(resolve_path(dir_path)).iterdir() if f.is_dir()]
+
+
+def count_files(dir_path: str | Path, ext: str = "*") -> int:
+    if dir_path is None:
+        return 0
+    dir_path = Path(dir_path).resolve().as_posix()
+    return len(glob.glob(f"{dir_path}/**/*.{ext}", recursive=False))
