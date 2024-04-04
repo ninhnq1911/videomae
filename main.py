3a5434ea8118309abb9b69931243abc9c5524d64
ninhnq1911 <ninhnguyendx779@gmail.com>
2024-04-05 00:05:22 +0700

    Add main process steps

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+from transformers import Trainer
+from mltools.utl.file_util import get_subfolders
+from videomae.train.dataset import load_test_set, load_train_set, load_val_set
+from videomae.train.evaluate import collate_fn, compute_metrics
+from videomae.train.model import get_image_processor, get_model
+from videomae.train.trainer import get_train_args
+from videomae.train.transform import init_train_transform, init_val_transform
+from videomae.train.utl import print_sample_info
+from videomae.config import clip_duration, dataset_dir, model_ckpt, model_local
+
+
+subset = get_subfolders(dataset_dir)
+cls_name = get_subfolders(os.path.join(dataset_dir, subset[0]))
+id2label = {i: cls for i, cls in enumerate(cls_name)}
+label2id = {cls: i for i, cls in id2label.items()}
+
+image_processor = get_image_processor(model_ckpt)
+model = get_model(model_local, label2id, id2label)
+
+train_transform = init_train_transform()
+val_transform = init_val_transform()
+
+train_dataset = load_train_set(dataset_dir, clip_duration, train_transform)
+val_dataset = load_val_set(dataset_dir, clip_duration, val_transform)
+test_dataset = load_test_set(dataset_dir, clip_duration, val_transform)
+
+# We can access the `num_videos` argument to know the number of videos we have in the
+# dataset.
+print(train_dataset.num_videos, val_dataset.num_videos, test_dataset.num_videos)
+
+sample_video = next(iter(train_dataset))
+print(sample_video.keys())
+print_sample_info(sample_video, id2label)
+
+train_args = get_train_args(train_dataset)
+trainer = Trainer(
+    model,
+    train_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    tokenizer=image_processor,
+    compute_metrics=compute_metrics,
+    data_collator=collate_fn,
+)
+
+trainer.train()
+trainer.evaluate(test_dataset)
+
+trainer.save_model("checkpoint")
+test_results = trainer.evaluate(test_dataset)
+trainer.log_metrics("test", test_results)
+trainer.save_metrics("test", test_results)
+trainer.save_state()
+
+trainer.push_to_hub()
